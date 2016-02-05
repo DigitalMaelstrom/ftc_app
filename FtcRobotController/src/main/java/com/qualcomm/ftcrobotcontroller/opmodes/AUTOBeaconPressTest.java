9917257d2f9e4ffd3e62151dc1ae748d2779c19a
Red Hot Techie Peppers <rhtp@learnscienceandmathclub.org>
2016-02-04 20:05:05 -0600

    worked on beacon press. The color sensors are weird in that they both present data in ic2channel 60.

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AUTOBeaconPressTest.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/AUTOBeaconPressTest.java
@@ -35,53 +35,55 @@ import android.graphics.Color;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.Servo;
 
 public class AUTOBeaconPressTest extends LinearOpMode {
   ColorSensor sensorRGB2;
   ColorSensor sensorRGB1;
-  public void runOpMode() throws InterruptedException {
-    sensorRGB1 = hardwareMap.colorSensor.get("mr");
-    sensorRGB1.enableLed(true);
-    sensorRGB2 = hardwareMap.colorSensor.get("mr2");
-    sensorRGB2.enableLed(true);
+  Servo servoa;
 
+  public void runOpMode() throws InterruptedException {
+    sensorRGB1 = hardwareMap.colorSensor.get("colorFront");
+    sensorRGB1.enableLed(false);
+    sensorRGB2 = hardwareMap.colorSensor.get("colorBack");
+    sensorRGB2.enableLed(false);
+    servoa = hardwareMap.servo.get("servoColor");
     waitOneFullHardwareCycle();
     waitForStart();
-      sensorRGB2.enableLed(false);
-      sensorRGB1.enableLed(false);
-      waitOneFullHardwareCycle();
-      sensorRGB2.enableLed(false);
-      sensorRGB1.enableLed(false);
 
 
-    float hsvValues2[] = {0F,0F,0F};
-    final float values2[] = hsvValues2;
 
-      sensorRGB2.enableLed(false);
+
+      float hsvValues2[] = {0F, 0F, 0F};
+      final float values2[] = hsvValues2;
+
+
+
+      float hsvValues[] = {0F, 0F, 0F};
+      final float values[] = hsvValues;
+    while (true) {
+      Color.RGBToHSV(sensorRGB1.red() * 8, sensorRGB1.green() * 8, sensorRGB1.blue() * 8, hsvValues);
+      telemetry.addData("Clear", sensorRGB1.alpha());
+      telemetry.addData("Red  ", sensorRGB1.red());
+      telemetry.addData("Green", sensorRGB1.green());
+      telemetry.addData("Blue ", sensorRGB1.blue());
+      telemetry.addData("Hue", hsvValues[0]);
       Color.RGBToHSV(sensorRGB2.red() * 8, sensorRGB2.green() * 8, sensorRGB2.blue() * 8, hsvValues2);
       telemetry.addData("Clear2", sensorRGB2.alpha());
       telemetry.addData("Red2  ", sensorRGB2.red());
       telemetry.addData("Green2", sensorRGB2.green());
       telemetry.addData("Blue2 ", sensorRGB2.blue());
       telemetry.addData("Hue2", hsvValues2[0]);
+      telemetry.addData("address1",sensorRGB1.getI2cAddress());
+      telemetry.addData("address2",sensorRGB2.getI2cAddress());
       waitOneFullHardwareCycle();
 
-    float hsvValues[] = {0F,0F,0F};
-    final float values[] = hsvValues;
-
-    sensorRGB1.enableLed(false);
-    Color.RGBToHSV(sensorRGB1.red() * 8, sensorRGB1.green() * 8, sensorRGB1.blue() * 8, hsvValues);
-    telemetry.addData("Clear", sensorRGB1.alpha());
-    telemetry.addData("Red  ", sensorRGB1.red());
-    telemetry.addData("Green", sensorRGB1.green());
-    telemetry.addData("Blue ", sensorRGB1.blue());
-    telemetry.addData("Hue", hsvValues[0]);
-    waitOneFullHardwareCycle();
-
-      telemetry.addData("WAITING", "waiting");
-      Thread.sleep(10000);
-      if (sensorRGB1.red() > sensorRGB1.blue())
-      {
+      if ((sensorRGB1.red() > sensorRGB1.blue()) && sensorRGB2.red() < sensorRGB2.blue()) {
+        servoa.setPosition(0);
       }
+      if ((sensorRGB1.red() < sensorRGB1.blue()) && sensorRGB2.red() > sensorRGB2.blue()) {
+        servoa.setPosition(1);
+      }
+    }
   }
 }
